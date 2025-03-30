19d45a76dfc2e61c795586ca8baec0df109d15c2
AndreyCavalcante <andrey.cavalcante06@gmail.com>
2025-03-30 01:27:38 -0300

    Commit inicial

---
+++ src/public/index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__ . '/../../vendor/autoload.php';
+
+    use Steampixel\Route;
+
+    define('VIEWS_PATH', __DIR__ . '/../views');
+
+    Route::add('/', function () {
+        require VIEWS_PATH . '/index.php';
+        exit;
+    }, ['get', 'post']);
+
+    Route::run('/');
+?>
