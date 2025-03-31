37616fab075a5882a8cc481e717bae2312f9813b
Andrey Cavalcante <andrey.cavalcante06@gmail.com>
2025-03-31 15:02:52 -0300

    corrigindo erro no index

--- src/Views/index.php
+++ src/Views/index.php
@@ -2,18 +2,18 @@
     use App\Lib\User;
 
     $user = new User();
-    $user->getUserById(1);
+    $userData = $user->getUserById(1);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-    <?echo $user['name'];?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <?echo $userData['name'];?>
+    </body>
 </html>
